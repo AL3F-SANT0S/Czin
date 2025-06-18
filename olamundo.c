73992ee3ad3ed51c6507dee07c3241024ba02b60
AL3F-SANT0S <213347442+AL3F-SANT0S@users.noreply.github.com>
2025-06-17 22:23:28 -0300

    Update olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 
+int main() {
+  int numero01;
+  int numero02;
+
+  numero01 = 10;
+  numero02 = 20;
+
+  if(numero01 > numero02) {
+    printf("Número 01 é Maior!\n")
+      }
+}
